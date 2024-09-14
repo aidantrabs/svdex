a982020aa06a82b1b1a3381356415b73d1e3bd01
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-09-14 02:35:05 -0400

    Update main and read image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use image::{ GenericImageView };
+
+fn decomposition(img: &str) {
+    let A = image::open(format!("{img}")).unwrap();
+    println!("{:?}", A.dimensions());
+}
+
 fn main() {
     println!("Hello, world!");
+    let img_name = "examples/picture.jpg";
+    decomposition(img_name);
 }
