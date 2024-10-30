1e9f175d4584e89e7870d3c29762d562339615a8
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-10-30 08:16:01 -0400

    Purge main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use image::{ GenericImageView };
-
-fn decomposition(img: &str) {
-    let A = image::open(format!("{img}")).unwrap();
-    let rgb = A.to_rgb8();
-    let chunks: Vec<&[u8]> = rgb.chunks(3).collect();
-
-    for chunk in &chunks {
-        println!("{:?}", chunk);
-    }
-}
-
 fn main() {
-    let img_name = "examples/picture.jpg";
-    decomposition(img_name);
+    
 }
