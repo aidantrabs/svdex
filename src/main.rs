8bfab24278f18263d923bf32436a8537c2612ec8
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-09 08:04:33 -0400

    feat: wire up cli with placeholder sucommand handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    
+use anyhow::Result;
+use clap::Parser;
+
+use svdex::cli::{Cli, Command};
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Info { image } => {
+            println!("info: {}", image.display());
+        }
+        Command::Compress { image, k, output } => {
+            println!("compress: {} k={} output={:?}", image.display(), k, output);
+        }
+        Command::Experiment { image, ranks } => {
+            println!("experiment: {} ranks={:?}", image.display(), ranks);
+        }
+    }
+
+    Ok(())
 }
